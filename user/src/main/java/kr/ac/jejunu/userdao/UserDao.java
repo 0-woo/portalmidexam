d930a6cbc2645a6e724c9d7fa34c130231d833e2
0-woo <dudndp678@naver.com>
2022-04-22 17:39:18 +0900

    2

--- user/src/main/java/kr/ac/jejunu/userdao/UserDao.java
+++ user/src/main/java/kr/ac/jejunu/userdao/UserDao.java
@@ -2,7 +2,7 @@ package kr.ac.jejunu.userdao;
 
 import java.sql.*;
 
-public class UserDao {
+public abstract class UserDao {
     public User get(Integer id) throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection connection =
@@ -43,9 +43,6 @@ public class UserDao {
         connection.close();
     }
 
-    private Connection getConnection() throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        return DriverManager.getConnection("jdbc:mysql://192.168.151.176:3306/jeju?serverTimezone=UTC"
-                , "jeju", "jejupw");
+    abstract public Connection getConnection() throws ClassNotFoundException, SQLException ;
+
     }
-}
